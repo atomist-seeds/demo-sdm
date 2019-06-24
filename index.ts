585116d88908eae52614c4cbfe4097d210a089cd
David Dooling <david@atomist.com>
2019-06-24 13:31:46 -0500

    Migrate to atomist/sdm-core/configure
    NOT DONE!!!
    
    Move goals and machine configuration to index.  Next step is to
    provide the goal sets and push rules as the data structure returned by
    the configure method.
    
    /cc @cdupuis

--- index.ts
+++ index.ts
@@ -14,62 +14,208 @@
  * limitations under the License.
  */
 
-import { Configuration } from "@atomist/automation-client";
-import { execPromise } from "@atomist/sdm";
 import {
+    AutoCodeInspection,
+    Autofix,
+    Cancel,
+    execPromise,
+    Fingerprint,
+    githubTeamVoter,
+    goals,
+    GoalWithFulfillment,
+    ImmaterialGoals,
+    IndependentOfEnvironment,
+    isMaterialChange,
+    not,
+    ProductionEnvironment,
+    PushImpact,
+    ToDefaultBranch,
+} from "@atomist/sdm";
+import {
+    configure,
     ConfigureOptions,
-    configureSdm,
+    githubGoalStatusSupport,
+    goalStateSupport,
     isGitHubAction,
+    k8sGoalSchedulingSupport,
+    Tag,
+    Version,
 } from "@atomist/sdm-core";
+import {
+    Build,
+    buildAwareCodeTransforms,
+} from "@atomist/sdm-pack-build";
+import { DockerBuild } from "@atomist/sdm-pack-docker";
+import { issueSupport } from "@atomist/sdm-pack-issue";
+import {
+    k8sSupport,
+    KubernetesDeploy,
+} from "@atomist/sdm-pack-k8s";
+import {
+    HasSpringBootApplicationClass,
+    HasSpringBootPom,
+    IsMaven,
+} from "@atomist/sdm-pack-spring";
 import * as appRoot from "app-root-path";
+import * as _ from "lodash";
 import * as path from "path";
-import { machine } from "./lib/machine/machine";
+import { AddDockerfile } from "./lib/commands/addDockerfile";
+import { addSpringSupport } from "./lib/machine/springSupport";
 
-const machineOptions: ConfigureOptions = {
-    requiredConfigurationValues: [
-        "sdm.docker.hub.registry",
-        "sdm.docker.hub.user",
-        "sdm.docker.hub.password",
-    ],
-};
+export const configuration = configure(async sdm => {
+    // Goals
+    const autofix = new Autofix();
+    const version = new Version();
+    const codeInspection = new AutoCodeInspection();
+    const fingerprint = new Fingerprint();
+    const pushImpact = new PushImpact();
+    const build = new Build();
+    const tag = new Tag();
+    const dockerBuild = new DockerBuild();
+    const stagingDeployment = new KubernetesDeploy({ environment: "testing" });
+    const productionDeployment = new KubernetesDeploy({
+        environment: "production",
+        preApproval: true,
+    });
+    const publish = new GoalWithFulfillment({
+        uniqueName: "Publish",
+        environment: IndependentOfEnvironment,
+        orderedName: "2-publish",
+        displayName: "publish",
+        workingDescription: "Publishing",
+        completedDescription: "Published",
+        failedDescription: "Published failed",
+        isolated: true,
+    });
+    const releaseArtifact = new GoalWithFulfillment({
+        uniqueName: "ReleaseArtifact",
+        environment: ProductionEnvironment,
+        orderedName: "3-release-artifact",
+        displayName: "release artifact",
+        workingDescription: "Releasing artifact",
+        completedDescription: "Released artifact",
+        failedDescription: "Release artifact failure",
+        isolated: true,
+    });
+    const releaseDocker = new GoalWithFulfillment({
+        uniqueName: "ReleaseDocker",
+        environment: ProductionEnvironment,
+        orderedName: "3-release-docker",
+        displayName: "release Docker image",
+        workingDescription: "Releasing Docker image",
+        completedDescription: "Released Docker image",
+        failedDescription: "Release Docker image failure",
+        isolated: true,
+    });
+    const releaseTag = new GoalWithFulfillment({
+        uniqueName: "ReleaseTag",
+        environment: ProductionEnvironment,
+        orderedName: "3-release-tag",
+        displayName: "create release tag",
+        completedDescription: "Created release tag",
+        failedDescription: "Creating release tag failure",
+    });
+    const releaseDocs = new GoalWithFulfillment({
+        uniqueName: "ReleaseDocs",
+        environment: ProductionEnvironment,
+        orderedName: "3-release-docs",
+        displayName: "publish docs",
+        workingDescription: "Publishing docs",
+        completedDescription: "Published docs",
+        failedDescription: "Publishing docs failure",
+        isolated: true,
+    });
+    const releaseVersion = new GoalWithFulfillment({
+        uniqueName: "ReleaseVersion",
+        environment: ProductionEnvironment,
+        orderedName: "3-release-version",
+        displayName: "increment version",
+        completedDescription: "Incremented version",
+        failedDescription: "Incrementing version failure",
+    });
+    const cancel = new Cancel({ goals: [autofix, build, dockerBuild, publish] });
 
-/* tslint:disable:no-invalid-template-strings */
-export const configuration: Configuration = {
-    postProcessors: [
-        async config => {
-            if (isGitHubAction()) {
-                config.environment = "gke-int-demo";
-                config.apiKey = "${ATOMIST_API_KEY}";
-                config.token = "${ATOMIST_GITHUB_TOKEN}";
-                config.sdm = {
-                    ...config.sdm,
-                    docker: {
-                        hub: {
-                            registry: "atomist",
-                            user: "${DOCKER_USER}",
-                            password: "${DOCKER_PASSWORD}",
+    // Goal sets
+    // Just running review and autofix
+    const checkGoals = goals("checks")
+        .plan(cancel, autofix, version, fingerprint, pushImpact)
+        .plan(codeInspection).after(autofix);
+    // Just running the build and publish
+    const buildGoals = goals("build")
+        .plan(build).after(autofix)
+        .plan(publish).after(build);
+    // Build including docker build
+    const dockerGoals = goals("docker build")
+        .plan(dockerBuild).after(build);
+    // Docker build and testing and production kubernetes deploy
+    const stagingDeployGoals = goals("deploy")
+        .plan(stagingDeployment).after(dockerBuild);
+    const productionDeployGoals = goals("prod deploy")
+        .plan(productionDeployment).after(stagingDeployment)
+        .plan(releaseArtifact, releaseDocker, releaseDocs, releaseTag, releaseVersion).after(productionDeployment);
+
+    sdm.addCodeTransformCommand(AddDockerfile);
+    addSpringSupport(sdm);
+    sdm.addGoalApprovalRequestVoter(githubTeamVoter());
+    sdm.addExtensionPacks(
+        buildAwareCodeTransforms({
+            buildGoal: build,
+            issueCreation: {
+                issueRouter: {
+                    raiseIssue: async () => { /* raise no issues */ },
+                },
+            },
+        }),
+        issueSupport(),
+        goalStateSupport(),
+        githubGoalStatusSupport(),
+        k8sGoalSchedulingSupport(),
+        k8sSupport({ addCommands: true }),
+    );
+}, {
+        name: "demo-sdm",
+        postProcessors: [
+            async config => {
+                _.merge(config, {
+                    sdm: {
+                        spring: {
+                            formatJar: path.join(appRoot.path, "bin", "spring-format-0.1.0-SNAPSHOT-jar-with-dependencies.jar"),
+                        },
+                        build: {
+                            tag: false,
+                        },
+                        cache: {
+                            enabled: true,
+                            path: "/opt/data",
                         },
                     },
-                };
-
-                await execPromise("git", ["config", "--global", "user.email", "\"bot@atomist.com\""]);
-                await execPromise("git", ["config", "--global", "user.name", "\"Atomist Bot\""]);
-
-            }
-            return config;
-        },
-        configureSdm(machine, machineOptions),
-    ],
-    sdm: {
-        spring: {
-            formatJar: path.join(appRoot.path, "bin", "spring-format-0.1.0-SNAPSHOT-jar-with-dependencies.jar"),
-        },
-        build: {
-            tag: false,
-        },
-        cache: {
-            enabled: true,
-            path: "/opt/data",
-        },
-    },
-};
+                });
+                return config;
+            },
+            async config => {
+                if (isGitHubAction()) {
+                    config.environment = "gke-int-demo";
+                    config.apiKey = "${ATOMIST_API_KEY}";
+                    config.token = "${ATOMIST_GITHUB_TOKEN}";
+                    config.sdm = {
+                        ...config.sdm,
+                        docker: {
+                            hub: {
+                                registry: "atomist",
+                                user: "${DOCKER_USER}",
+                                password: "${DOCKER_PASSWORD}",
+                            },
+                        },
+                    };
+                    await execPromise("git", ["config", "--global", "user.email", "\"bot@atomist.com\""]);
+                    await execPromise("git", ["config", "--global", "user.name", "\"Atomist Bot\""]);
+                }
+                return config;
+            },
+        ],
+        requiredConfigurationValues: [
+            "sdm.docker.hub.registry",
+            "sdm.docker.hub.user",
+            "sdm.docker.hub.password",
+        ],
+    });
